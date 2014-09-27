2562a7e402b326d3878fcf17b4021e11612da2de
Timur Abishev <abishev.timur@gmail.com>
2014-09-27 17:34:28 +0400

    minor (2)

---
+++ resources/public/js/compiled/out/rockets/finish.js
@@ -0,0 +1,5 @@
+// Compiled by ClojureScript 0.0-2202
+goog.provide('rockets.finish');
+goog.require('cljs.core');
+
+//# sourceMappingURL=finish.js.map
---
+++ resources/public/js/compiled/out/rockets/game.js
@@ -0,0 +1,5 @@
+// Compiled by ClojureScript 0.0-2202
+goog.provide('rockets.game');
+goog.require('cljs.core');
+
+//# sourceMappingURL=game.js.map
